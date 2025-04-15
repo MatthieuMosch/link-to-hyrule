6e0e72587b7f8b371369df2266e0c0d64f64de2a
MatthieuMosch <mmosch@gmail.com>
2025-04-15 16:19:31 +0200

    basic routing toegevvoegd

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import NotFound from "./pages/notfound/NotFound.jsx";
 
 function App() {
     return (
         <>
-            Edit <code>src/App.jsx</code> and save to test HMR
+            <header>logo and navigation</header>
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </main>
+            <footer>disclaimer</footer>
         </>
     )
 }
