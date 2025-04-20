a4449375a508b062f63e3230c3202a1072903ede
MatthieuMosch <mmosch@gmail.com>
2025-04-20 15:55:29 +0200

    nav afhankelijk gemaakt van authenticatie. voor buttons gekozen ipv navlink vanwege de logout functionaliteit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer>
-            <h2>disclaimer</h2>
+            <a href="https://store.nintendo.nl/nl/the-legend-of-zelda-breath-of-the-wild-000000000002520048">Zelda BotW</a>
         </footer>
     );
 }
